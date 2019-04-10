7f7eba40f4ce91a535bd1354f7779463c86191ba
beannama <ben.nar.95@gmail.com>
2019-04-10 17:58:29 -0400

    only left user

---
+++ project_1/db/migrate/20190410214746_create_admins.rb
@@ -0,0 +1,16 @@
+class CreateAdmins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :admins do |t|
+      t.string :username
+      t.string :password
+      t.text :bio
+      t.string :email
+      t.string :geofence
+      t.string :location
+      t.float :longitude
+      t.float :latitude
+
+      t.timestamps
+    end
+  end
+end
