bbabe8adafb70b280db2a9901b956282b3779131
beannama <ben.nar.95@gmail.com>
2019-04-10 17:33:15 -0400

    superadmn model

---
+++ project_1/db/migrate/20190410213158_create_super_admins.rb
@@ -0,0 +1,15 @@
+class CreateSuperAdmins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :super_admins do |t|
+      t.string :email
+      t.string :password
+      t.float :latitude
+      t.float :longitude
+      t.string :username
+      t.text :bio
+      t.reference :post
+
+      t.timestamps
+    end
+  end
+end
